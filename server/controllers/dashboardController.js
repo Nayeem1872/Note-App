5d141a654d246f91ec077423c175db325960650f
“Nayeem1872” <nayeem.islam.1872@gmail.com>
2023-01-17 02:29:13 +0600

    modified:   server/controllers/dashboardController.js 	new file:   server/models/Notes.js 	modified:   views/dashboard/index.ejs

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,13 +1,53 @@
+const { default: mongoose } = require('mongoose');
+const Note = require('../models/Notes');
+
+
+
 exports.dashboard =async(req,res )=>{
+    let perPage = 12;
+        let page = req.query.page || 1
+
     const locals={
         title:"Dashboard",
         description:"Free NodeJS Notes"
     }
+    try {
+        Note.aggregate([
+            {
+                $sort:{
+                    createdAt:-1
+                }
+            },
+            {$match:{user:mongoose.Types.ObjectId(req.user.id)}},
+            {
+                $project:{
+                    title:{ $substr:['$title', 0, 30]},
+                    body:{ $substr:['$title', 0, 100]},
+                }
+            }
 
-    res.render('dashboard/index',{
-        locals,
-        layout:'../views/layouts/dashboard',
-        name: req.user.name    
+        ]).skip(perPage * page- perPage)
+        .limit(perPage)
+        .exec(function(err,notes){
+            Note.count().exec(function(err,count){
+                if(err) return next(err);
+            }); 
+        })
 
-    }); 
+
+       
+        res.render('dashboard/index',{
+            locals,
+            notes,
+            layout:'../views/layouts/dashboard',
+            name: req.user.name    
+    
+        }); 
+    } catch (error) {
+        
+    }
+
+
+
+   
 }
